4ccffb89031eb9788157b8ae8b2f0e217e6654f2
Faisal Hossain <faisal.hossain75@gmail.com>
2023-11-07 11:19:20 -0600

    init files

---
+++ arrayLists/template.py
@@ -0,0 +1,28 @@
+class ArrayLists:
+    """lorem ipsum
+
+    Attributes:
+        backing_array (list): lorem ipsum
+        initial_capacity (int): lorem ipsum
+        size (int): lorem ipsum
+    """
+
+    def __init__(self):
+        """Initialize a new instance ArrayLists with lorem ipsum.
+        """
+        backing_array = []
+        initial_capacity = 9
+        size = 0
+
+    def add_to_front(self, data):
+        """Adds the data to the front of the list.
+
+        Notes: This method may require shifting elements, and is expected to run in O(n) time complexity.
+
+        :param any data: the data to add to the front of the list
+        :raises ValueError: if data is null
+        """
+        pass
+
+
+
